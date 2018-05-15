9ad615ebd2cb7736004fb4162a714c38af0a3aca
kowal11baggio <kowal11baggio@gmail.com>
2018-05-15 20:29:20 +0200

    Commit before change mid

--- HMM/Algorithm/KerncelDPSence.cs
+++ HMM/Algorithm/KerncelDPSence.cs
@@ -19,7 +19,7 @@ namespace HMM.Algorithm
         private double[,] _matrixB;
         private double[] _matrixPi;
 
-        private double[] _delta;
+        private double[,] _delta;
         private double[,] _paths;
         private double[,] _prec;
 
@@ -27,7 +27,9 @@ namespace HMM.Algorithm
 
         public KerncelDPSence()
         {
-
+            _matrixA = null;
+            _matrixB = null;
+            _matrixPi = null;
         }
 
         public void initialization()
@@ -40,9 +42,54 @@ namespace HMM.Algorithm
             _stateSequence.setLenghtStatesSequence(_numberOfObservationsM);
             _numberOfStatesN = States.Instance.getNumberOfStates();
             _lenghtSequenceT = ObservationSequence.Instance.getLenghtObservation();
-            _delta = new double[_numberOfObservationsM];
-            _paths = new double[_lenghtSequenceT, _numberOfStatesN];
-            _prec = new double[_lenghtSequenceT, _numberOfStatesN];
+            _paths = new double[_numberOfStatesN, _lenghtSequenceT];
+            _prec = new double[_numberOfStatesN, _lenghtSequenceT];
+            _delta = new double[_lenghtSequenceT, _numberOfStatesN];
+        }
+        public void algorithm()
+        {
+            for (int i = 0; i < _numberOfStatesN; i++)
+            {
+                if (_matrixPi[i] == 0.0)
+                    _matrixPi[i] = double.Epsilon;
+                _delta[0, i] = Math.Log(_matrixPi[i] * _matrixB[i, (int)ObservationSequence.Instance.getObservationSequence()[0]]);
+                _paths[i,0] = i;
+                _prec[i,0] = -1;
+            }
+            for (int t = 1; t < _lenghtSequenceT; t++)
+            {
+                for (int i = 0; i < _numberOfStatesN; i++)
+                {
+                    double _maximum = double.NegativeInfinity;
+                    for (int j = 0; j < _numberOfStatesN; j++)
+                    {
+                        double result = _delta[t - 1, j] + Math.Log(_matrixA[j, i]) + Math.Log(_matrixB[i, (int)ObservationSequence.Instance.getObservationSequence()[t]]);
+                        if (result > _maximum)
+                        {
+                            _maximum = result;
+                            _delta[t, i] = result;
+                            _prec[i,0] = _paths[j,0];
+                        }
+                    }
+                }
+                for (int i = 0; i < _numberOfStatesN; i++)
+                {
+                    _paths[i,0] = _prec[i,0] + i;
+                }
+            }
+            int a = 0;
+            double best_path = double.NegativeInfinity;
+            int last_state = 0;
+            for (int i = 0; i < _numberOfStatesN; i++)
+            {
+                double result = _delta[_lenghtSequenceT - 1, i];
+                if (result > best_path)
+                {
+                    best_path = result;
+                    last_state = i;
+                }
+            }
+            Console.Write(_paths[last_state,0]);
         }
     }
 }
--- HMM/Algorithm/RunClass.cs
+++ HMM/Algorithm/RunClass.cs
@@ -11,12 +11,15 @@ namespace HMM.Algorithm
     public class RunClass
     {
         KernelHMMSence _kernelHMMSence;
+        KerncelDPSence _kerncelDPSence;
 
         public RunClass()
         {
             _kernelHMMSence = new KernelHMMSence();
+            _kerncelDPSence = new KerncelDPSence();
             initialization();
             HMMSenceAlgorithm();
+            DPSenceAlgorithm();
         }
 
         public void initialization()
@@ -33,6 +36,11 @@ namespace HMM.Algorithm
             _kernelHMMSence.aflaPass();
             _kernelHMMSence.betaPass();
             _kernelHMMSence.sequenceState();
+        }
+        public void DPSenceAlgorithm()
+        {
+            _kerncelDPSence.initialization();
+            _kerncelDPSence.algorithm();
         }
     }
 }
